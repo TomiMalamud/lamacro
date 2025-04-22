2bbe9052afda518a027cc7b1751ef956c11784c3
Tomás Malamud <121351375+TomiMalamud@users.noreply.github.com>
2025-04-22 20:59:41 -0300

    Update page.tsx

--- app/carry-trade/page.tsx
+++ app/carry-trade/page.tsx
@@ -13,7 +13,7 @@ export default async function CarryTradePage() {
   ]);
 
   return (
-    <main className="container mx-auto px-16 py-8">
+    <main className="container mx-auto px-6 md:px-16 py-8">
       <h1 className="text-3xl font-bold mb-2">Carry Trade</h1>
       <p className="text-muted-foreground mb-8">
         Fijate cuál es el mejor bono para hacer carry trade. Se actualiza casi a tiempo real. Inspirado en el gran <a href="https://x.com/JohnGalt_is_www/status/1912555971400069372" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4 hover:decoration-stone-900 hover:text-stone-900 dark:hover:decoration-stone-200 dark:hover:text-stone-200 transition-all duration-300">JohnGalt_is_www</a>. Usa la API de <a href="https://data912.com" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4 hover:decoration-slate-200 hover:text-slate-200 dark:hover:decoration-slate-400 dark:hover:text-slate-400 transition-all duration-300">Milton Casco</a>.
