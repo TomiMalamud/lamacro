2052aee0bbf808663d9b01be4986b51ef94a4ee9
TomiMalamud <tomasmalamud@gmail.com>
2025-08-03 20:27:38 -0300

    remove nomberFlow for variable detail

--- components/bcra/variable-detail-client.tsx
+++ components/bcra/variable-detail-client.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { BCRAVariable } from "@/lib/bcra-fetch";
-import NumberFlow from "@number-flow/react";
+import { formatNumber } from "@/lib/utils";
 import { useState } from "react";
 import { VariableTimeSeriesChart } from "./variable-time-series-chart";
-import { formatNumber } from "@/lib/utils";
 
 export function VariableDetailClient({
   initialValue,
@@ -18,24 +17,16 @@ export function VariableDetailClient({
 }) {
   const [percentChange, setPercentChange] = useState<number | null>(null);
 
-  const percentValue =
-    percentChange !== null ? Math.round((percentChange ?? 0) * 100) / 10000 : 0;
-
   return (
     <>
       <div className="flex items-center gap-3">
         <div className="text-3xl font-bold">{formatNumber(initialValue)}</div>
         {percentChange !== null && (
-          <NumberFlow
-            value={percentValue}
-            locales="es-AR"
-            format={{
-              style: "percent",
-              maximumFractionDigits: 2,
-              signDisplay: "always",
-            }}
-            className="text-lg"
-          />
+          <span>
+            {percentChange > 0
+              ? `+${formatNumber(percentChange, 2)}%`
+              : `${formatNumber(percentChange, 2)}%`}
+          </span>
         )}
       </div>
 
